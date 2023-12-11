128728b8091ba07bf1b63409385d6b0d7b2648a7
Rob Ryan <rob@affclicks.com>
2023-12-11 19:51:42 +1100

    Parallel test/ github workflow

--- days_test.go
+++ days_test.go
@@ -36,10 +36,15 @@ var expectations = map[string]string{
 
 func TestDays(t *testing.T) {
 	for day, expect := range expectations {
-		cmd := exec.Command(fmt.Sprintf("bin/%s", day))
-		out, err := cmd.CombinedOutput()
+		expect := expect
+		day := day
+		t.Run(day, func(t *testing.T) {
+			t.Parallel()
+			cmd := exec.Command(fmt.Sprintf("bin/%s", day))
+			out, err := cmd.CombinedOutput()
 
-		assert.NoError(t, err)
-		assert.Equal(t, expect, strings.TrimRight(string(out), "\n"), fmt.Sprintf("Day %s", day))
+			assert.NoError(t, err)
+			assert.Equal(t, expect, strings.TrimRight(string(out), "\n"), fmt.Sprintf("Day %s", day))
+		})
 	}
 }
